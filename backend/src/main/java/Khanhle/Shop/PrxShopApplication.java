12cd80cbcff0bfaa0eb157f5138453c5ab2c0062
khanh <khanhle0209c@gmail.com>
2026-01-30 15:01:09 +0700

    [UPDATE] Download dependencies and setup fe/be

---
+++ backend/src/main/java/Khanhle/Shop/PrxShopApplication.java
@@ -0,0 +1,13 @@
+package Khanhle.Shop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrxShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrxShopApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/Khanhle/Shop/PrxShopApplicationTests.java
@@ -0,0 +1,13 @@
+package Khanhle.Shop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrxShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
