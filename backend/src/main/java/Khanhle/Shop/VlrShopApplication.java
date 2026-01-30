6d0818c4086e968f478a0b85565176f5386b2260
khanh <khanhle0209c@gmail.com>
2026-01-30 15:43:49 +0700

    [FIX] Refactor name of shop

---
+++ backend/src/main/java/Khanhle/Shop/VlrShopApplication.java
@@ -0,0 +1,13 @@
+package Khanhle.Shop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VlrShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VlrShopApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/Khanhle/Shop/VlrShopApplicationTests.java
@@ -0,0 +1,13 @@
+package Khanhle.Shop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VlrShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
